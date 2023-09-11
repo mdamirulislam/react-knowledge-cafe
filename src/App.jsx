047b95768877bdde413c2939e222a9fa0f8d1fe6
mdamirulislam <mdamirulislam.prime@gmail.com>
2023-09-11 21:11:17 +0600

    make blog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Blogs/>
+      <div className='md:flex'>
+        <Blogs />
+        <Bookmarks />
+      </div>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import img from "../../images/profile.png"
 const Header = () => {
     return (
-        <div>
+        <header className="flex items-center justify-between p-4 mx-4 border-b-2">
             <h1 className="text-4xl font-bold">Hello from header</h1>
-        </div>
+            <img src={img} alt="" />
+        </header>
     );
 };
 
