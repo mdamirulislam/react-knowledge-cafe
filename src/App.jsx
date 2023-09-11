6764f15a5e547b3e9c4a5cd2500173d9292a8fbc
mdamirulislam <mdamirulislam.prime@gmail.com>
2023-09-11 19:40:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Blogs/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Hello from header</h1>
+        </div>
+    );
+};
+
+export default Header;
